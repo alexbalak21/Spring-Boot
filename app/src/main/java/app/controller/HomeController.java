8c72360925d8955bb8e75b2c610b8096000b935d
Alex Balak <alex.balak@outlook.com>
2025-10-13 13:13:08 +0200

    minimal app with java 21

--- app/src/main/java/app/controller/HomeController.java
+++ app/src/main/java/app/controller/HomeController.java
@@ -1,7 +1,5 @@
 package app.controller;
 
-import jakarta.servlet.http.HttpServletResponse;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(HttpServletResponse response) {
-        // content-type: application/json
-        response.setContentType("application/json");
-        
-        return "{\"message\" : \"Hello from Spring Boot!\"}";
+    public String home() {
+        return "Hello from Spring Boot";
     }
-
     
 }
