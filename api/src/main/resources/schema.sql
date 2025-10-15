e31bb71d6de989ddc756f6673553f9b37662ba22
Alex Balak <alex.balak@outlook.com>
2025-10-15 13:20:34 +0200

    working crud on Product with front end.

--- api/src/main/resources/schema.sql
+++ api/src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 -- Drop the existing table if it exists
 -- DROP TABLE IF EXISTS products;
 
--- Recreate the table with the new schema
-CREATE TABLE products (
+--CREATE TABLE products IF NOT EXISTS
+CREATE TABLE IF NOT EXISTS products (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     price DOUBLE NOT NULL,
